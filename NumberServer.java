d0a67a7436ffc0652015f08e036985bb9403358b
evawlve <dimarquez@ucsd.edu>
2023-04-21 14:23:57 -0700

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -4,21 +4,18 @@ import java.net.URI;
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+     String str = "";
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Diego's Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
+            return str;
         } else {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    str += (parameters[1]) + "\n";
+                    return str;
                 }
             }
             return "404 Not Found!";
